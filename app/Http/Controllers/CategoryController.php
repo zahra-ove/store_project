f65d2801c7e24603086a5bb161ccec7f26960bd2
zahra_ove <zahra.oveyedzade@gmail.com>
2024-06-08 09:32:10 +0330

    Complete CategoryController logic

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,51 +3,40 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\storeCategoryRequest;
+use App\Http\Requests\updateCategoryRequest;
 use App\Http\Resources\CategoryResource;
 use App\Models\Category;
-use App\Models\fs;
-use Illuminate\Http\Request;
+
 
 class CategoryController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         return CategoryResource::collection(Category::all());
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(storeCategoryRequest $request, CategoryResource $categoryResource)
     {
         $category = Category::create($request->validated());
         return $categoryResource->make($category);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(int $id)
+    public function show(int $id, CategoryResource $categoryResource)
     {
         $category = Category::findOrFail($id);
+        return $categoryResource->make($category);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Category $category)
+    public function update(updateCategoryRequest $request, int $id, CategoryResource $categoryResource)
     {
-        //
+        $validated = $request->validated();
+        $category = Category::findOrFail($id)->update($validated);
+        return $categoryResource->make($category>refresh());
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Category $category)
+    public function destroy(int $id)
     {
-        //
+        Category::where('id', $id)->delete();
+        return response()->noContent();
     }
 }
